f07a51897799eda43fa766e4b0f458c19c5b3f1e
Ezra Chung <ezra.chung97@gmail.com>
2023-11-20 19:44:07 -0800

    Initial setup

--- INFO.md
+++ INFO.md
@@ -1,9 +1,17 @@
 ## Objective
-- Create application to store yardages and suggest golf positions and hold a record of your score
-- Create handicap
+### Stater app
+- Application will store yardages, suggest clubs, hold scores against par, and create handicap
+    - User can input estimated yardages they reached. 
+        - Percentage of swing and based on that the 100% yardage will be revealed. 
+        - Input if downwing or against wind
 
 ## Challenges
 - Finding and utilizing a golf api that gets different courses and gets the scorecard
 - Expensive to get the actual yardages
     - so idea is to put in yardage and be given a suggestion
     - So may be more similar to a notepad
+- Make this more mobile friendly
+
+
+## Future Updates and Objectives can be 
+- Playing with friend but just getting the scores. Having a season long score count
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
+const calculateScore = (course: number, handicap: number): number => {
+ const parScore = 72;
+ return parScore - ((course - handicap) * 5 / 9);
+};
+
+const App: React.FC = () => {
+ const [course, setCourse] = useState('');
+ const [handicap, setHandicap] = useState('');
+ const [score, setScore] = useState('');
+
+ const handleCalculate = () => {
+    const c = parseInt(course);
+    const h = parseInt(handicap);
+    if (isNaN(c) || isNaN(h)) {
+      setScore('Error: Invalid input');
+    } else {
+      setScore(calculateScore(c, h).toFixed(2));
+    }
+ };
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Handicap Score Count</h1>
+      <input
+        type="number"
+        placeholder="Course Rating"
+        value={course}
+        onChange={(e) => setCourse(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Handicap Index"
+        value={handicap}
+        onChange={(e) => setHandicap(e.target.value)}
+      />
+      <button onClick={handleCalculate}>Calculate</button>
+      <h2>{score}</h2>
     </div>
-  );
-}
+ );
+};
 
 export default App;
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
