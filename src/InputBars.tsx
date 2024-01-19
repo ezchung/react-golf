f1d263004dca23520ef1c0e39206ec15ce313943
Ezra Chung <ezra.chung97@gmail.com>
2024-01-18 19:27:20 -0800

    Reorganize components

---
+++ src/InputBars.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+/**
+ * Will be taking in type, placeholder, value, function
+ * 
+ * InputBar is where the person inputs their solutions
+ * - Remember I have to hold that value and put it into the database
+ */
+
+const InputBar: React.FC = () => {
+    return <div></div>
+}
+
+export default InputBar
